bba4b742db9203a78f3249f22e8025e60669a290
Joshua Sun <joshuapsun@gmail.com>
2025-05-29 10:03:24 -0400

    added styling to time picker to be more responsive

--- src/components/custom/timePicker.tsx
+++ src/components/custom/timePicker.tsx
@@ -53,33 +53,35 @@ export function TimePickerWithRange({ className, timeRange, setTimeRange }: Time
     setTimeRange(newTimeRange);
   }
 
-  return (<div className={cn("flex gap-2", className)}>
-    <Select value={`${timeRange.from}`} onValueChange={handleStartingTimeUpdate}>
-      <SelectTrigger className="w-fit">
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Start Time</SelectLabel>
-          {[...Array(24).keys()].map((i) => {
-            return (<SelectItem key={i} value={`${i}`}>{format(new Date(1970, 0, 0, i), "p")}</SelectItem>)
-          })}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-    <p className="text-nowrap text-sm py-2"> to </p>
-    <Select value={`${timeRange.to}`} onValueChange={handleEndingTimeUpdate}>
-      <SelectTrigger className="w-fit">
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>End Time</SelectLabel>
-          {[...Array(24).keys()].map((i) => {
-            return (<SelectItem key={i} value={`${i + 1}`}>{format(new Date(1970, 0, 0, i + 1), "p")}</SelectItem>)
-          })}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  </div>)
+  return (
+    <div className={cn("flex flex-col sm:flex-row items-center gap-2 md:gap-4", className)}>
+      <Select value={`${timeRange.from}`} onValueChange={handleStartingTimeUpdate}>
+        <SelectTrigger className="w-full sm:w-fit md:text-lg md:px-6 md:py-4">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent className="md:w-[200px]">
+          <SelectGroup>
+            <SelectLabel className="md:text-lg">Start Time</SelectLabel>
+            {[...Array(24).keys()].map((i) => {
+              return (<SelectItem key={i} value={`${i}`} className="md:text-lg md:py-2">{format(new Date(1970, 0, 0, i), "p")}</SelectItem>)
+            })}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <p className="text-nowrap text-sm md:text-lg flex items-center"> to </p>
+      <Select value={`${timeRange.to}`} onValueChange={handleEndingTimeUpdate}>
+        <SelectTrigger className="w-full sm:w-fit md:text-lg md:px-6 md:py-4">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent className="md:w-[200px]">
+          <SelectGroup>
+            <SelectLabel className="md:text-lg">End Time</SelectLabel>
+            {[...Array(24).keys()].map((i) => {
+              return (<SelectItem key={i} value={`${i + 1}`} className="md:text-lg md:py-2">{format(new Date(1970, 0, 0, i + 1), "p")}</SelectItem>)
+            })}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  )
 }
